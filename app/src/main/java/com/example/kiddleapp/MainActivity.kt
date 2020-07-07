2826f8c941d62eda86c20109858b5d169786b558
Hummam Aly Baziyad <huhummamalybaziyad@gmail.com>
2020-07-07 14:45:31 +0700

    mengubah activity menjadi fragment
    home menjadi main activity

--- app/src/main/java/com/example/kiddleapp/MainActivity.kt
+++ app/src/main/java/com/example/kiddleapp/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.kiddleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.fragment.app.Fragment
+import com.example.kiddleapp.fragment.Fragment_Beranda
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val navListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when(item.itemId) {
+            R.id.menu_beranda -> {
+                val fragment = Fragment_Beranda()
+                addFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+
+        false
+    }
+
+    private fun addFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit()
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottom_navigation.setOnNavigationItemSelectedListener(navListener)
+        val fragment = Fragment_Beranda()
+        addFragment(fragment)
+
     }
 }
---
+++ app/src/main/java/com/example/kiddleapp/fragment/Fragment_Beranda.kt
@@ -0,0 +1,47 @@
+package com.example.kiddleapp.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.kiddleapp.R
+import com.synnapps.carouselview.CarouselView
+import com.synnapps.carouselview.ImageClickListener
+import com.synnapps.carouselview.ImageListener
+import kotlinx.android.synthetic.main.fragment_beranda.*
+
+class Fragment_Beranda : Fragment() {
+
+    var sampleImages = intArrayOf(R.drawable.slide_1, R.drawable.slide_2)
+
+    var imageListener: ImageListener = object : ImageListener {
+        override fun setImageForPosition(position: Int, imageView: ImageView) {
+            // You can use Glide or Picasso here
+            imageView.setImageResource(sampleImages[position])
+            imageView.scaleType = ImageView.ScaleType.FIT_XY
+        }
+    }
+
+    var clickListener: ImageClickListener = object :ImageClickListener {
+        override fun onClick(position: Int) {
+            Toast.makeText(activity, "Clicked $position", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return  inflater.inflate(R.layout.fragment_beranda, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val carouselView = carouselView
+        carouselView.pageCount = sampleImages.size
+        carouselView.setImageListener(imageListener)
+        carouselView.setImageClickListener(clickListener)
+
+    }
+}
