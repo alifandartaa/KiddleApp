8c155058775d9ef2fa0c4c1f54d5ca5f2b06113b
Alif Andarta <alifandartaa@yahoo.com>
2020-07-13 02:01:32 +0700

    delete import gradle glide

--- app/src/main/java/com/example/kiddleapp/OnBoardingFragment.kt
+++ app/src/main/java/com/example/kiddleapp/OnBoardingFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.fragment_on_boarding.*
 
 // TODO: Rename parameter arguments, choose names that match
